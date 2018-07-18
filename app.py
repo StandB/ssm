fb4c6cf153989ba6b7938fbbdd5b36ed6c859c35
StandB <standb@protonmail.com>
2018-07-18 21:10:57 +0200

    user adding and avatars

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-import json
-import bcrypt
+import json, bcrypt, uuid, os
 from flask import Flask, request, jsonify, \
-    render_template, redirect, url_for, g
+    render_template, redirect, url_for, g, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_user, logout_user, current_user, \
     login_required, LoginManager
+from werkzeug import secure_filename
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = r'C:\Users\Stan\dev\home\avatars'
 app.config['SECRET_KEY'] = 'ye'
 
 db = SQLAlchemy(app)
@@ -27,14 +28,16 @@ class User(db.Model):
     id = db.Column('id', db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    avatar = db.Column(db.String(100), nullable=False)
     posts = db.relationship('Post', backref='user', lazy='dynamic')
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, avatar):
         self.username = username
         self.password = bcrypt.hashpw(
             password.encode('utf-8'),
             bcrypt.gensalt()
         ).decode('utf8')
+        self.avatar = avatar
 
     def is_authenticated(self):
         return True
@@ -51,7 +54,8 @@ class User(db.Model):
     @property
     def serialize(self):
         """ convert the object to JSON """
-        return {'id': self.id, 'username': self.username}
+        return {'id': self.id, 'username': self.username, 'avatar': self.avatar}
+
 
 
 class Post(db.Model):
@@ -74,19 +78,32 @@ class Post(db.Model):
 @app.route('/api/users', methods=['POST'])
 def add_user():
     """ add a user """
-    user_json = request.json
-    user = User(user_json['username'], user_json['password'])
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if password is None or username is None:
+        return jsonify({'error': 'Username and password please!'})
+
+    avatar = 'default.jpg'
+    print(request.files)
+    f = request.files.get('file')
+    if f is not None:
+        avatar = uuid.uuid4().hex + ".jpg"  
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], avatar))
+
+    user = User(username, password, avatar)
     db.session.add(user)
     db.session.commit()
-    return jsonify(user.serialize)
+    return redirect(url_for('get_users'))
 
 
 @login_required
 @app.route('/api/posts', methods=['POST'])
 def create_post():
     """ create post """
-    title = request.form['title']
-    content = request.form['content']
+    title = request.form.get('title')
+    content = request.form.get('content')
+    if title is '' or content is '':
+        return redirect(url_for('create_post_page'))
     post = Post(title, content)
     post.user = g.user
     db.session.add(post)
@@ -164,6 +181,27 @@ def create_post_page():
 def get_profile():
     return render_template('profile.html')
 
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/adduser', methods=['GET'])
+@login_required
+def add_user_page():
+    return render_template('adduser.html')
+
+
+def create_root_user():
+    user = db.session.query(User).filter_by(username='root').first()
+    if user is not None:
+        return
+    me = User('root', 'root', 'default.jpg')
+    db.session.add(me)
+    db.session.commit()
+
+
 if __name__ == '__main__':
     db.create_all()
+    create_root_user()
     app.run(debug=True)
